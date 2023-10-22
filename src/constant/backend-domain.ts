db750418d3cdcf17842aa3000a63c7f5457ba7b1
sangtrandev00 <nhatsang0101@gmail.com>
2023-10-22 10:21:49 +0700

    modified backend domain,...

--- src/constant/backend-domain.ts
+++ src/constant/backend-domain.ts
@@ -1,5 +1,5 @@
 // export const BACKEND_URL = 'https://fullstack-techspace-es6-api.onrender.com';
 
-export const BACKEND_URL = 'rest-api-techspace.trannhatsang.com';
+export const BACKEND_URL = 'https://rest-api-techspace.trannhatsang.com';
 
 // export const BACKEND_URL = 'http://localhost:8080'; // Development mode!!!
